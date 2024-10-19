858500731926a8399547561f2002a782482839a2
Fatih ŞAHİNBAŞ <fsahinbas@gmail.com>
2024-10-19 18:02:29 +0300

    header components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
+import Header from "./components/header/header";
 function App() {
-  return <>Home</>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./header.module.css";
+import { Link } from "react-router-dom";
+import About from "../../pages/About";
+import Vans from "../../pages/Vans";
+const header = () => {
+  return (
+    <header className={styles.header}>
+      <p className={styles.logo}>#VANLIFE</p>
+      <nav className={styles.nav}>
+        <Link to="/about" element={<About />} className={styles.link}>
+          About
+        </Link>
+        <Link to="/vans" element={<Vans />} className={styles.link}>
+          Vans
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default header;
