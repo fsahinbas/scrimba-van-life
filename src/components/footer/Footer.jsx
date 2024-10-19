c5d55b7d4e59745d877c809da773ebb0d7fa17aa
Fatih ŞAHİNBAŞ <fsahinbas@gmail.com>
2024-10-19 18:36:33 +0300

    footer text changed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./footer.module.css";
 const Footer = () => {
-  return <footer className={styles.footer}>&copy; 2022 #VANLIFE</footer>;
+  return <footer className={styles.footer}>&copy; 2024 #VANLIFE</footer>;
 };
 
 export default Footer;
