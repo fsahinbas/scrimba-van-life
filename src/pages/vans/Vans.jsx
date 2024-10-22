44e99fe1d87b477a7d4ff7a16bf812beb4961c70
Fatih ŞAHİNBAŞ <fsahinbas@gmail.com>
2024-10-22 16:32:19 +0300

    filter buttons added

--- src/pages/vans/Vans.jsx
+++ src/pages/vans/Vans.jsx
@@ -1,24 +1,54 @@
 import React from "react";
 import styles from "./vans.module.css";
 import Card from "../../components/card/Card";
+import { useSearchParams } from "react-router-dom";
 
 const Vans = () => {
   const [vans, setVans] = React.useState(null);
+  const [searchParams, setSearhParams] = useSearchParams();
+
   React.useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
       .then((data) => setVans(data.vans));
   }, []);
 
-  const renderVans = vans && vans.map((van) => <Card key={van.id} {...van} />);
+  if (!vans) {
+    return <h2>Loading...</h2>;
+  }
+  const filteredVans = searchParams.get("type")
+    ? vans.filter((van) => van.type === searchParams.get("type"))
+    : vans;
+
+  const renderVans =
+    filteredVans && filteredVans.map((van) => <Card key={van.id} {...van} />);
   return (
     <>
       <h1 className={styles.title}>Explore our van options</h1>
       <div className={styles.filters}>
-        <div className={styles.filter}>Simple</div>
-        <div className={styles.filter}>Luxury</div>
-        <div className={styles.filter}>Rugged</div>
-        <a href="#">Clear filters</a>
+        <button
+          className={`${styles.filter} ${styles.simple}`}
+          onClick={() => setSearhParams({ type: "simple" })}
+        >
+          Simple
+        </button>
+        <button
+          className={`${styles.filter} ${styles.luxury}`}
+          onClick={() => setSearhParams({ type: "luxury" })}
+        >
+          Luxury
+        </button>
+        <button
+          className={`${styles.filter} ${styles.rugged}`}
+          onClick={() => setSearhParams({ type: "rugged" })}
+        >
+          Rugged
+        </button>
+        {searchParams.get("type") && (
+          <button onClick={() => setSearhParams({})} className={styles.clear}>
+            Clear filters
+          </button>
+        )}
       </div>
       <div className={styles.vans}>{renderVans}</div>
     </>
