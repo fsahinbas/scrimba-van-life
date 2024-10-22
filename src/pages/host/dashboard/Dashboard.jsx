b3ca49ce6beeaa7c823dcc6131c6586e39b558a1
Fatih ŞAHİNBAŞ <fsahinbas@gmail.com>
2024-10-22 16:36:04 +0300

    .

--- src/pages/host/dashboard/Dashboard.jsx
+++ src/pages/host/dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import styles from "./Dashboard.module.css";
+import styles from "./dashboard.module.css";
 const Dashboard = () => {
   return (
     <>
