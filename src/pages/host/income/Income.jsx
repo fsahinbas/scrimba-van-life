0edda873a17cdf363127f2b5c194682d7aca3442
Fatih ŞAHİNBAŞ <fsahinbas@gmail.com>
2024-10-27 22:42:15 +0300

    income added

--- src/pages/host/income/Income.jsx
+++ src/pages/host/income/Income.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 import styles from "./income.module.css";
 const Income = () => {
-  return <div>Income</div>;
+  const transactionsData = [
+    { amount: 720, date: "Jan 3, '23", id: "1" },
+    { amount: 560, date: "Dec 12, '22", id: "2" },
+    { amount: 980, date: "Dec 3, '22", id: "3" },
+  ];
+  return (
+    <section className={styles.income}>
+      <h1>Income</h1>
+      <p>
+        Last <span>30 days</span>
+      </p>
+      <h2>$2,260</h2>
+      <img
+        className={styles.graph}
+        src="/assets/images/income-graph.png"
+        alt="Income graph"
+      />
+      <div className={styles.header}>
+        <h3>Your transactions (3)</h3>
+        <p>
+          Last <span>30 days</span>
+        </p>
+      </div>
+      <div className={styles.transactions}>
+        {transactionsData.map((item) => (
+          <div key={item.id} className={styles.transaction}>
+            <h3>${item.amount}</h3>
+            <p>{item.date}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Income;
